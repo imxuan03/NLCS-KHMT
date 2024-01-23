613a48ed9efc43ac1a30a49b404881f56afe4b27
imxuan03 <txuan0164@gmail.com>
2024-01-23 09:34:15 +0700

    tuan 5

--- controllers/admin/flight.controller.js
+++ controllers/admin/flight.controller.js
@@ -2,7 +2,7 @@ const Flight = require("../../models/flight.model");
 
 const filterStatusHelper = require("../../helpers/filter-status");
 const searchHelper = require("../../helpers/search");
-
+const paginationHelper = require("../../helpers/pagination");
 // [GET] /admin/flights 
 module.exports.index = async (req, res) => {
     const filterStatus = filterStatusHelper(req.query);
@@ -20,13 +20,102 @@ module.exports.index = async (req, res) => {
         find.title = objectSearch.regex;
     }
 
+    //Pagination
+    let initPagination = {
+        currentPage : 1,
+        limitItems: 4
+    }
+    const countProducts = await Flight.countDocuments(find);
+    const objectPagination = paginationHelper(initPagination, req.query, countProducts);
 
-    const flights = await Flight.find(find);
+    //End Pagination
+
+    const flights = await Flight.find(find)
+        .limit(objectPagination.limitItems)
+        .skip(objectPagination.skip);
 
     res.render("admin/pages/flights/index",{
         pageTitle: "Dịch Vụ Chuyến Bay",
         flights:flights,
         filterStatus:filterStatus,
         keyword:objectSearch.keyword,
+        pagination: objectPagination
+    });
+}
+
+
+// [PATCH] /admin/flights/change-status/:status/:id
+module.exports.changeStatus = async (req, res) => {
+    const status = req.params.status;
+    const id = req.params.id;
+
+    await Flight.updateOne({ _id: id }, { status: status });
+
+    req.flash('success', 'Cập nhật trạng thái thành công!');
+
+    res.redirect("back");
+}
+
+// [PATCH] /admin/flights/change-multi
+module.exports.changeMulti = async (req, res) => {
+    //Cách viết 1 
+    const type = req.body.type;
+    const ids = req.body.ids.split(", ");
+    //Cách viết 2
+    // const  { type, ids} = req.body;
+
+    // console.log(type);
+    // console.log(ids);
+
+    switch (type) {
+        case "active":
+        case "inactive":
+            await Flight.updateMany({ _id: { $in: ids } }, { status: type })
+            req.flash('success', `Cập nhật trạng thái thành công ${ids.length} sản phẩm!`);
+            break;
+        case "delete-all":
+            await Flight.updateMany({ _id: { $in: ids } }, {
+                deleted: true,
+                deletedAt: new Date()
+            })
+            req.flash('success', `Xóa trạng thái thành công ${ids.length} sản phẩm!`);
+            break;
+        case "change-position":
+            for (const item of ids) {
+                // console.log(item.split("-"));
+                const [id, position] = item.split("-");
+                // console.log(id);
+                // console.log(position);
+                await Flight.updateOne({ _id: id }, { position: position });
+            }
+            req.flash('success', `Thay đổi vị trí thành công ${ids.length} sản phẩm!`);
+            break;
+        default:
+            break;
+    }
+
+    res.redirect("back");
+}
+
+// [DELETE] /admin/flights/delete/:id
+module.exports.deleteItem = async (req, res) => {
+    const id = req.params.id;
+
+    //xóa vĩnh viễn
+    //await Product.deleteOne({_id: id});
+    //xóa mềm
+    await Flight.updateOne({ _id: id }, {
+        deleted: true,
+        deletedAt: new Date()
+    });
+    req.flash('success', `Xóa trạng thái thành công sản phẩm!`);
+    res.redirect("back");
+
+}
+
+// [GET] /admin/flights/create
+module.exports.create = async (req, res) => {
+    res.render("admin/pages/flights/create",{
+        pageTitle: "Tạo mới chuyến bay",
     });
 }
--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const express = require('express')
 const mongoose = require('mongoose');
+const methodOverride = require('method-override'); //dùng để override PATCH, PUT,DELETE...
+const bodyParser = require('body-parser')
+const flash = require('express-flash')
+const cookieParser = require('cookie-parser')
+const session = require('express-session')
+
 
 require('dotenv').config() //gọi file .env
 
@@ -11,19 +17,25 @@ const database = require("./config/database");
 database.connect(); //Gọi hàm connect để kết nối database
 
 
-
-// const Flight = mongoose.model('Flight', {
-//   title: String,
-//   description: String,
-//   price: Number
-// })
-
 const port = process.env.PORT;
 const app = express();
 
 app.set('views', './views')
 app.set('view engine', 'pug')
 app.use(express.static('public'))
+// ví dụ:  override with POST having ?_method=DELETE
+app.use(methodOverride('_method'))
+
+// flash 
+app.use(cookieParser('NHAKDLZNX'));
+app.use(session({ cookie: { maxAge: 60000 }}));
+app.use(flash());
+// end flash 
+
+// parse application/x-www-form-urlencoded
+//https://www.npmjs.com/package/body-parser
+app.use(bodyParser.urlencoded({ extended: false }))
+
 
 //Variables
 app.locals.prefixAdmin = systemConfig.prefixAdmin;
