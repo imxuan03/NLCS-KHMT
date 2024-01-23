9a31cbd43d37832a73d06f9b616da052ce68ca30
imxuan03 <txuan0164@gmail.com>
2024-01-23 20:38:13 +0700

    chỉnh front end trang chủ

--- controllers/client/flight.controller.js
+++
@@ -1,6 +0,0 @@
-// [GET] /fligts 
-module.exports.index = (req, res) => {
-    res.render("client/pages/flights/index",{
-        pageTitle: "Dịch Vụ Chuyến Bay"
-    });
-}
--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -1,10 +1,7 @@
-const flightRoutes = require("./flight.route");
 const homeRoutes = require("./home.route");
 
 module.exports = (app) => {
 
     app.use('/', homeRoutes)
-      
-    app.use('/flights', flightRoutes)
       
 }
