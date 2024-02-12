589a663d049619cce2e1f5282e285177115dba27
imxuan03 <txuan0164@gmail.com>
2024-02-12 13:59:39 +0700

    Đăng nhập đăng xuất Admin

---
+++ middlewares/admin/authAdmin.middleware.js
@@ -0,0 +1,17 @@
+const Account = require("../../models/account.model");
+const systemConfig = require("../../config/system");
+
+module.exports.requireAuthAdmin = async (req, res, next) => {
+    const user = await Account.findOne({
+        token: req.cookies.token
+    })
+
+
+    if(user.role_id != "admin"){
+        req.flash('error', `Chỉ có tài khoản Quản Trị Viên được truy cập!`);
+        res.redirect(`/${systemConfig.prefixAdmin}/dashboard`);
+        return;
+    }
+
+    next();
+}
--- routes/admin/index.route.js
+++ routes/admin/index.route.js
@@ -1,15 +1,33 @@
 const systemConfig = require("../../config/system")
 
+const authMiddleware = require("../../middlewares/admin/auth.middleware");
+const authAdminMiddleware = require("../../middlewares/admin/authAdmin.middleware");
+
+
 const dashboardRoutes = require("./dashboard.route")
 const flightRoutes = require("./flight.route")
 const accountRoutes = require("./account.route")
+const authRoutes = require("./auth.route")
 
 module.exports = (app)=>{
 
     const PATH_ADMIN =  "/" + systemConfig.prefixAdmin ;   //"/admin"
 
-    app.use(PATH_ADMIN+"/dashboard", dashboardRoutes);
-    app.use(PATH_ADMIN+"/flights", flightRoutes);
-    app.use(PATH_ADMIN+"/accounts", accountRoutes);
+    app.use(
+        PATH_ADMIN+"/dashboard", 
+        authMiddleware.requireAuth, //middleware kiểm tra tài khoản để có thể truy cập vào
+        dashboardRoutes
+    );
+    app.use(
+        PATH_ADMIN+"/flights",
+        authMiddleware.requireAuth, 
+        flightRoutes
+    );
+    app.use(PATH_ADMIN+"/accounts",
+        authMiddleware.requireAuth,
+        authAdminMiddleware.requireAuthAdmin,
+        accountRoutes
+    );
+    app.use(PATH_ADMIN+"/auth", authRoutes);
     
 }
