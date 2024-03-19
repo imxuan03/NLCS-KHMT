f4867f97b8f3a50399e527cab6a7916b560b48f6
imxuan03 <txuan0164@gmail.com>
2024-03-19 09:55:49 +0700

    thêm phần quản lí order

--- controllers/admin/order.controller.js
+++ controllers/admin/order.controller.js
@@ -1,6 +1,29 @@
+const Order = require("../../models/order.model");
+const Flight = require("../../models/flight.model");
+
+
+const systemConfig = require("../../config/system");
 // [GET] /admin/orders 
-module.exports.index =  (req, res) => {
+module.exports.index = async  (req, res) => {
+
+    const records = await Order.find({})
+
+
+    let totalPrice = 0;
+    for (const record of records) {
+        let totalPrice = 0;
+        for (const flight of record.flights) {
+            const flightInfo = await Flight.findOne({ _id: flight.flight_id });
+            if (flightInfo && flightInfo.title) {
+                flight.flightName = flightInfo.title;
+            }
+            totalPrice += flight.quantity*flight.price;
+        }
+        record.totalPrice=totalPrice;
+    }
+
     res.render('admin/pages/orders/index', {
-        pageTitle: "Trang Chủ"
+        pageTitle: "Quản lý đơn vé",
+        records: records,
     })
 }
